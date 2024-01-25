1d1b9cbbdd051140070be3563e90204f09f31ff0
Stephen Turley <stephen@turley.dev>
2024-01-25 17:03:12 -0500

    removed commented code

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,7 @@ fn main() {
     }
     let rom_path = &args[1];
 
-    // let mut heap: [u8; 4096] = [0x0; 4096];
-    // let mut pc: u16 = heap::ROM_START;
-    // let _i: u16 = 0;
-    // let _stack: [u16; 64];
-    // let _frame_buffer: [[bool; 64]; 32];
-    // let _delay: u8;
-    // let _sound: u8;
-    // let _registers: [u8; 16] = [0; 16];
-    //
     let mut system: System = System::init(rom_path);
-
     loop {
         // fetch
         let op = system.fetch();
