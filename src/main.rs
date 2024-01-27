7e4c6a8fac3535bbf59359ee350706bdbebd7b8e
Stephen Turley <stephen@turley.dev>
2024-01-27 15:09:11 -0500

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::time::Instant;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        panic!("You need to pass a path to a ROM file to load");
+        panic!("Missing ROM path argument example: ./chip8 roms/ibm.ch8");
     }
     let rom_path = &args[1];
 
@@ -21,18 +21,15 @@ fn main() {
         // run at 60hz
         if cycle_start.elapsed().as_micros() >= 16_600 {
             cycle_start = Instant::now();
-            // fetch
+
             let op = system.fetch();
 
-            // decode
             let op_code: OpCode = op_code::decode(op);
             if op_code == OpCode::Unknown {
                 break;
             }
 
-            //execute
             system.execute(&op_code);
         }
     }
-    println!("End of program");
 }
